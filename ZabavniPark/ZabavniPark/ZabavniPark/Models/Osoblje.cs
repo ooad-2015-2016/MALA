facb7385006f7493fa2d4bc27098de8615d8a8e3
Milan Zuza <mzuza1@etf.unsa.ba>
2016-04-23 15:13:18 +0200

    Osoblje.cs OsobljeDbContext.cs

---
+++ ZabavniPark/ZabavniPark/ZabavniPark/Models/Osoblje.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp_OOAD.ParkBaza.Models
+{
+    public enum TipOsoblja { Radnik, SalterRadnik, Administrator };
+    class Osoblje
+    {
+      
+        String ime;
+        String prezime;
+        TipOsoblja tip;
+        String username;
+        String password;
+        int radniStaz;
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public TipOsoblja Tip
+        {
+            get
+            {
+                return tip;
+            }
+
+            set
+            {
+                tip = value;
+            }
+        }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public int RadniStaz
+        {
+            get
+            {
+                return radniStaz;
+            }
+
+            set
+            {
+                radniStaz = value;
+            }
+        }
+    }
+}
