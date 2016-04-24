b7533d94664a00a9035a40fc147a4d7394745851
Milan Zuza <mzuza1@etf.unsa.ba>
2016-04-24 22:08:29 +0200

    Signed-off-by: Milan Zuza <mzuza1@etf.unsa.ba>

---
+++ ZabavniPark/ZabavniPark/ZabavniPark/Models/DefaultPodaciOsoblje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZabavniPark.ZabavniPark.Models
+{
+    class DefaultPodaciOsoblje
+    {
+      /*  public static void Initialize(OsobljeDbContext context)
+        {
+            if (!context.Osoblje.Any())
+            {
+                context.Osoblje.AddRange(
+                new Osoblje()
+                {
+                    ime = "Milan",
+                    prezime = "Zuza",
+                    tip = "Administrator",
+                    username = "mzuza1",
+                    password = "SerenaWilliams",
+                    radnistaz = 15
+                    
+                }
+                );
+                context.SaveChanges();
+            }
+        }
+        */
+    }
+}
