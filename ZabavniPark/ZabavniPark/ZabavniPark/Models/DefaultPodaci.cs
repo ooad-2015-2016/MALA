90816f7d595d04d61c97c179cc01823eeb97ecb6
Milan Zuza <mzuza1@etf.unsa.ba>
2016-04-26 17:21:50 +0200

    dodan DefaultPodaci.cs

---
+++ ZabavniPark/ZabavniPark/ZabavniPark/Models/DefaultPodaci.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZabavniPark.ZabavniPark.Models;
+namespace ZabavniPark.ZabavniPark.Models
+{
+    class DefaultPodaci
+    {
+        public enum StanjeAtrakcije { Operating, NotOperating };
+        public enum TipOsoblja { Radnik, SalterRadnik, Administrator };
+       // public enum TipPosjetilaca { Obicni, Registrovani, Gold }
+        public static void Initialize(ZabavniParkDbContext context)
+        {
+            if (!context.Atrakcije.Any())
+            {
+                context.Atrakcije.AddRange(new MyApp_OOAD.AtrakcijaBaza.Models.Atrakcija()
+                {
+                    fourSqaureId = "Id",
+                    Naziv = "Voz smrti",
+                    Kapacitet = 200,
+                    VrijemeOtvaranja = TimeSpan.Zero,
+                    VrijemeZatvaranja = TimeSpan.Zero,
+                    TrenutniBrojPosjetilaca = 56,
+                    Stanje = MyApp_OOAD.AtrakcijaBaza.Models.StanjeAtrakcije.Operating,
+                    Cijena = 50,
+                    BrojNaCekanju = 2
+                });
+            }
+            if (!context.Osoblja.Any())
+            {
+                context.Osoblja.AddRange(new MyApp_OOAD.ParkBaza.Models.Osoblje()
+                {
+                    ime = "Milan",
+                    prezime = "Zuza",
+                    tip = MyApp_OOAD.ParkBaza.Models.TipOsoblja.Administrator,
+                    username = "mzuza1",
+                    password = "zmilan1",
+                    radniStaz = 5
+                });
+            }
+
+            if (!context.Posjetioci.Any())
+            {
+                context.Posjetioci.AddRange(new Posjetilac()
+                {
+                    Ime = "Adnan",
+                    Prezime = "Dzelihodzic",
+                    tip = TipPosjetilaca.Gold
+
+                });
+            }
+            context.SaveChanges();
+
+
+        }
+    }
+}
